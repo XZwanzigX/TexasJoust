798d0954c5da77438cae072f5ac385852d53cbda
Foxfire <foxfire@evilcupcakes.net>
2012-03-05 18:02:58 -0600

    Mock pages.

---
+++ TexasJoust/homeMock.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Lysts on the Lake 2012</title>
+    </head>
+
+    <body style="background-image: url('Master/Home2.jpg'); background-position: top center; background-repeat: no-repeat;"">
+        <div style="position:absolute; top:0px; left: 0px; height: 2000px;">
+            &nbsp;
+        </div>
+    </body>
+</html>
--- TexasJoust/otherPageMock.php
+++ TexasJoust/otherPageMock.php
@@ -2,7 +2,9 @@
     <head>
         <title>Lysts on the Lake 2012 - Other pages</title>
     </head>
-    <body style="background-image: url('Master/pieces/OtherPageBG.jpg'); /*background-position: center;*/ background-repeat: no-repeat;">
-          <img src="Master/pieces/menu/social/fb.png" style="position:absolute; top:230px; left: 600px;"/>
+    <body style="background-image: url('Master/pieces/OtherPageBG.jpg'); background-position: top center; background-repeat: no-repeat;">
+        <div style="position:absolute; top:0px; left: 0px; height: 2000px;">
+            <img src="Master/pieces/menu/social/fb.png" style="position:absolute; top:230px; left: 600px;"/>
+        </div>
     </body>
 </html>
